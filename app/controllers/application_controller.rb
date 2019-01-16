8db4324f00c2e38b70fc365ee2ed260a552d08fd
carly <carly@noukod.com>
2019-01-16 19:42:14 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,16 +17,28 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/signup" do
-		#your code here!
-	end
+  user = User.new(:username => params[:username], :password => params[:password])
+ 
+  if user.save
+    redirect "/login"
+  else
+    redirect "/failure"
+  end
+end
 
 	get "/login" do
 		erb :login
 	end
 
-	post "/login" do
-		#your code here!
-	end
+	post "/signup" do
+  user = User.new(:username => params[:username], :password => params[:password])
+ 
+  if user.save
+    redirect "/login"
+  else
+    redirect "/failure"
+  end
+end
 
 	get "/success" do
 		if logged_in?
