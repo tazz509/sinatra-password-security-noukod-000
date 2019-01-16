7ee3041f9106c10ff617f0c56008469666a3d401
carly <carly@noukod.com>
2019-01-16 19:42:39 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
 	post "/signup" do
   user = User.new(:username => params[:username], :password => params[:password])
- 
+
   if user.save
     redirect "/login"
   else
@@ -32,7 +32,7 @@ end
 
 	post "/signup" do
   user = User.new(:username => params[:username], :password => params[:password])
- 
+
   if user.save
     redirect "/login"
   else
